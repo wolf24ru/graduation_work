890554b09a971194e7e878d3318c3fee4d9f8317
Nikita <wolf24ru@gmail.com>
2022-04-19 17:27:13 +0700

    add location app

---
+++ demo_work/location/models.py
@@ -0,0 +1,38 @@
+from django.utils.translation import gettext_lazy as _
+from django.db import models
+
+
+class Region(models.Model):
+    region = models.CharField(_('region'), max_length=150)
+
+    class Meta:
+        verbose_name = _('Region')
+        verbose_name_plural = _('Region\'s list')
+
+    def __str__(self):
+        return f'{self.region}'
+
+
+class City(models.Model):
+    city = models.CharField(_('city'), max_length=100)
+
+    class Meta:
+        verbose_name = _('City')
+        verbose_name_plural = _('City\'s list')
+
+    def __str__(self):
+        return f'{self.city}'
+
+
+class RegionCity(models.Model):
+    region = models.ForeignKey(Region,
+                               verbose_name=_('Region'),
+                               related_name='region_rc',
+                               on_delete=models.CASCADE)
+    city = models.ForeignKey(City,
+                             verbose_name=_('City'),
+                             related_name='city_rc',
+                             on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self}'
