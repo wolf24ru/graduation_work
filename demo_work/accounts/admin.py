d0848e939b93380d82ba302031b8e6dba5a2db0a
Nikita <wolf24ru@gmail.com>
2022-04-19 17:24:20 +0700

    new db for accounts

--- demo_work/accounts/admin.py
+++ demo_work/accounts/admin.py
@@ -4,24 +4,24 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import CustomUser
 
-
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ['username', 'email', 'phone_number']
-    fieldsets = (('Редактирование пользователя',
-                  {'fields': ('username', 'email', 'phone_number',
-                              'is_staff', 'is_active')
-                   }
-                  ),
-                 )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'phone_number', 'is_staff', 'is_active')
-        }),
-    )
-
-
-admin.site.register(CustomUser, CustomUserAdmin)
+#
+# class CustomUserAdmin(UserAdmin):
+#     add_form = CustomUserCreationForm
+#     form = CustomUserChangeForm
+#     model = CustomUser
+#     list_display = ['username', 'email', 'phone_number']
+#     fieldsets = (('Редактирование пользователя',
+#                   {'fields': ('username', 'email', 'phone_number',
+#                               'is_staff', 'is_active')
+#                    }
+#                   ),
+#                  )
+#     add_fieldsets = UserAdmin.add_fieldsets + (
+#         (None, {
+#             'classes': ('wide',),
+#             'fields': ('email', 'phone_number', 'is_staff', 'is_active')
+#         }),
+#     )
+#
+#
+# admin.site.register(CustomUser, CustomUserAdmin)
--- demo_work/accounts/models.py
+++ demo_work/accounts/models.py
@@ -1,90 +1,170 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils.translation import gettext_lazy as _
 from django.db import models
+from demo_work.location.models import City, Region
+
+from validator import PhoneNumberValidator
+
+TYPE_USER = (
+    ('shop', 'Магазин'),
+    ('buyer', 'Покупатель'),
+)
 
 
-# class Permissions(models.Model):
-#     '''
-#     Класс для получения разрешений.
-#
-#     status: название статуса
-#     input_admin: наличие доступа в админку
-#     update: возможность изменять позиции
-#     payment: возможность оплачивать покупки
-#     write_comment: возможность писать коментарии(открывается только
-#     после выполнения заказа)
-#     delete: возможность удалять позиции
-#     bun: забаненный пользователь
-#     '''
-#     status = models.CharField(max_length=30)
-#     input_admin = models.BooleanField()
-#     update = models.BooleanField()
-#     payment = models.BooleanField()
-#     write_comment = models.BooleanField()
-#     delete = models.BooleanField()
-#     bun = models.BooleanField()
-#
-# # class Basket(models.Model):
-# #     ...
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        """
+        Создание пользователя
+        :param email: email
+        :param password: пароль
+        :param kwargs: дополнительные значения
+        :return: user
+        """
+        if not email:
+            raise ValueError('Email is not set. Please set email')
+        user = self.model(
+            email=self.normalize_email(email),
+            **kwargs
+        )
+        user.is_staff = False
+        user.is_superuser = False
+        user.set_password(password)
+        user.save(user=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **kwargs):
+        """
+        Создание супер пользователя
+        :param email: email
+        :param password: пароль
+        :param kwargs: дополнительные значения
+        :return: user
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            **kwargs
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
 
 class CustomUser(AbstractUser):
-    '''
+    """
     Класс пользователя.
-
-    nikname: никнайм пользователя
 
     email: email
     password: пороль
-    phone_number: номер телефона
-    permissions: разрешения из таблицы Permissions
+    company: Наименование компании
+    position: Должность в компании
+    username: Имя пользователя
+    is_active: Рабочая учетная запись
+    is_admin: Администратор
+    type: Тип пользователя
+    """
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['email', 'type']
+    objects = UserManager()
 
-    Возможно в них нет необходимости, а получать доступ у ним через смежные
-    таблицы
-    order: список заказов из тиблицы Order
+    email = models.EmailField(_('Email adres'),unique=True)
+    company = models.CharField(_('Company'), max_length=100, blank=True)
+    position = models.CharField(_('position'), max_length=100, blank=True)
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        _('user name'),
+        max_length=150,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _('A user with this name already exist'),
+        }
+    )
+    is_active = models.BooleanField(
+        _('Active'),
+        default=True,
+        help_text=_('Indicates that the user can use the account'
+                    'Unselect this instead of deleting accounts'
+                    ),
+        )
+    is_admin = models.BooleanField(
+        _('Admin'),
+        default=False,
+        help_text=_('Indicates that the user is superuser'),
+    )
+    type = models.CharField(
+        _('user type'),
+        choices=TYPE_USER,
+        max_length=5,
+        default='buyer',
+        help_text=_('choose your role in  between   shop and buyer'),
+    )
 
-    реализовать чуть позже
-    bonus: список бонусов пользователя и тиблице Bonus
-    basket: корзина пользователя, должна хранить добавленные в нее позиции и
-    очещаться после покупки
-    '''
+    def __str__(self):
+        return f'{self.username} {self.company} {self.type}'
 
-    username = models.CharField('Никнейм', max_length=58, unique=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField('Пароль', max_length=200)
-    phone_number = models.CharField('Номер телефона', max_length=12, unique=True)
-    # id_permission = models.ForeignKey(Permissions,
-    #                                    verbose_name='Разрешение',
-    #                                    related_name='permission',
-    #                                   related_query_name='custom_user',
-    #                                   on_delete=models.DO_NOTHING,
-    #                                   blank=True,
-    #                                   null=True
-    #                                   )
-    # bonus = ...
-    # order = ...
-
-    # basket = ...
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Список пользователей'
+        ordering = ('email',)
 
 
-class UserInfo(models.Model):
-    '''
-        Класс описывающий пользователя.
+class Contact(models.Model):
+    """
+        Контакты пользвоателя
 
-        name: имя пользователя
-        surname: фамилия пользователя
-        country: страна проживания
-        region: регион проживания
-        city: город проживания
-    '''
-    id_custom_user = models.OneToOneField(CustomUser,
-                                          on_delete=models.CASCADE,
-                                          primary_key=True,
-                                          unique=True,
-                                          related_name='user',
-                                          related_query_name='user_info'
-                                          )
-    name = models.CharField('Имя', max_length=58, blank=True)
-    surname = models.CharField('Фамилия', max_length=80, blank=True)
-    country = models.CharField('Страна', max_length=58, blank=True)
-    region = models.CharField('Регион', max_length=58, blank=True)
-    city = models.CharField('Город', max_length=58, blank=True)
+        region: Регион
+        city: Город
+        street: улица
+        house: дом
+        structure: Корпус
+        building: здание
+        apartment: квартира или офис
+        phone_validator: номер телефона
+    """
+
+    user = models.ForeignKey(CustomUser, verbose_name=_('User'),
+                             related_name='contacts', blank=True,
+                             on_delete=models.CASCADE)
+    # country = models.CharField('Страна', max_length=58, blank=True)
+    region = models.ForeignKey(Region,
+                               verbose_name=_('Region'),
+                               related_name='contacts',
+                               on_delete=models.CASCADE)
+    city = models.ForeignKey(City,
+                             verbose_name=_('City'),
+                             related_name='contacts',
+                             on_delete=models.CASCADE)
+    street = models.CharField(_('Street'), max_length=100)
+    house = models.CharField(_('House'), max_length=15, blank=True)
+    structure = models.CharField('Корпус', max_length=10, blank=True)
+    building = models.CharField(_('building'), max_length=10, blank=True)
+    apartment = models.CharField('Кв/Оф', max_length=10, blank=True)
+    phone_validator = PhoneNumberValidator()
+
+    class Meta:
+        verbose_name = _('user contacts')
+        verbose_name_plural = _('list of user contacts')
+
+
+class Shop(models.Model):
+    """
+    Магазин
+
+    url: сайт магазина
+    name: название магазина
+    user: пользователь(администратор) закрепелнный за магазином
+    order_accepting: показатель может ли магазин принемать заказы
+    """
+
+    url = models.URLField('URL', null=True, blank=True)
+    name = models.CharField(_('shop\'s name'), max_length=58)
+    user = models.ForeignKey(CustomUser, verbose_name=_('User'),
+                             related_name='shop', blank=True,
+                             null=True, on_delete=models.CASCADE)
+    order_accepting = models.BooleanField(
+        _('order receving status'),
+        default=True,
+        help_text=_('indicates the shop\'s ability to accept orders')
+    )
