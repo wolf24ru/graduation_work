3f90f5834520376ea736480845d128ae20340634
Nikita <wolf24ru@gmail.com>
2022-04-21 15:05:34 +0700

    add migrations and other changing

--- demo_work/accounts/admin.py
+++ demo_work/accounts/admin.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-
-from .forms import CustomUserChangeForm, CustomUserCreationForm
-from .models import CustomUser
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+#
+# from .forms import CustomUserChangeForm, CustomUserCreationForm
+# from .models import CustomUser
 
 #
 # class CustomUserAdmin(UserAdmin):
