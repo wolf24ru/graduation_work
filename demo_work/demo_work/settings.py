64dcb433ac1a4a86560a739202dc7aeee30f97ec
Nikita <wolf24ru@gmail.com>
2022-04-22 15:37:56 +0700

    add SessionAuthentication

--- demo_work/demo_work/settings.py
+++ demo_work/demo_work/settings.py
@@ -138,6 +138,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
