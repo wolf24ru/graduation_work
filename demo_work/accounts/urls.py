6d3fbce22ce3d36e60b65a3dca74e1744613472a
Nikita <wolf24ru@gmail.com>
2022-04-28 15:11:41 +0700

    add shove shop views

--- demo_work/accounts/urls.py
+++ demo_work/accounts/urls.py
@@ -1,6 +1,6 @@
 
 # from rest_framework.routers import DefaultRouter
-from accounts.views_api import UserFilling, RegistrationUser
+from accounts.views_api import UserFilling, RegistrationUser, ShopView
 #
 # router = DefaultRouter()
 # router.register('user_info', UserInfo, 'user_info')
@@ -14,4 +14,6 @@ urlpatterns = [
     # path('user_info/', UserInfo.as_view(), name='user_info'),
     path('user_filling/', UserFilling.as_view(), name='user_filling'),
     path('registration/', RegistrationUser.as_view(), name='registration'),
+    path('shops/', ShopView.as_view(), name='shops'),
+
 ]
--- demo_work/accounts/views_api.py
+++ demo_work/accounts/views_api.py
@@ -1,19 +1,12 @@
-from django.core.validators import URLValidator
-from requests import get
 from accounts.models import Shop, CustomUser
-from category.models import Category, CategoryShop
-from product.models import ProductInfo, Product, Parameter, ProductParameter
-from django.contrib.auth import authenticate
 from django.contrib.auth.password_validation import validate_password
-from django.core.exceptions import ValidationError
-from rest_framework.authtoken.models import Token
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.views import APIView
-from yaml import load as load_yaml, Loader
-from accounts.serializers import UserSerializer
+
+from accounts.serializers import UserSerializer, ShopSerializer
 from rest_framework.authtoken.models import Token
 
 from django.http import JsonResponse
@@ -54,9 +47,12 @@ class RegistrationUser(APIView):
             JsonResponse({'Error': 'unexpected argument'})
 
 
-
-
 class UserFilling(APIView):
+    """Информация о пользователи
+
+    GET: Получить информацию о пользователи
+    POST: изменить информацию о пользователи
+    """
     permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
@@ -77,6 +73,11 @@ class UserFilling(APIView):
         else:
             return JsonResponse({'Errors': user_serializer.errors})
 
+
+class ShopView(ListAPIView):
+    permission_classes = [AllowAny]
+    queryset = Shop.objects.filter(order_accepting=True)
+    serializer_class = ShopSerializer
 #
 # class UserInfo(APIView):
 #     permission_classes = [IsAuthenticated]
@@ -97,45 +98,3 @@ class UserFilling(APIView):
 #         }
 #         return JsonResponse(content)
 
-
-class UpdateCatalog(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return JsonResponse({'Error': 'Login required'}, status=401)
-        if request.user.type != 'shop':
-            return JsonResponse({'Error': 'User\'s type "shop" only'}, status=401)
-        url = request.data['url']
-        if url:
-            validate_url = URLValidator()
-            try:
-                validate_url(url)
-            except ValidationError as e:
-                return JsonResponse({'Error': str(e)}, status=400)
-            else:
-                stream = get(url).content
-                data = load_yaml(stream, Loader=Loader)
-
-                shop = Shop.objects.get_or_create(name=data['shop'], user_id=request.user.id)
-                for category in data['categories']:
-                    category_object = Category.objects.get_or_create(id=category['id'],
-                                                                     category=category['name'])
-                    category_shop = CategoryShop.object.get_or_create(category_id=category_object.id,
-                                                                      shop_id=shop.id)
-                ProductInfo.objects.filter(shop_id=shop.id).delete()
-                for item in data['goods']:
-                    product = Product.odjects.get_or_create(name=item['name'],
-                                                            category_id=item['category'])
-                    product_info = ProductInfo.odjects.get_or_create(product_id=product.id,
-                                                                     external_id=item['id'],
-                                                                     shop_id=shop.id,
-                                                                     price=item['price'],
-                                                                     quantity=item['quantity'])
-                    for name, value in item['parameters'].items():
-                        parameter_object = Parameter.objects.get_or_create(name=name)
-                        ProductParameter.objects.create(product_info_id=product_info.id,
-                                                        parameter_id=parameter_object.id,
-                                                        value=value)
-                return JsonResponse({'Msg': 'all create'}, status=201)
-        return JsonResponse({'Errors': 'Url error. Not specified basic arguments'})
