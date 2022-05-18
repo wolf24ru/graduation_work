916eeff927c4247e2dac77abc3f3f8919bc83517
Nikita <wolf24ru@gmail.com>
2022-05-18 12:14:50 +0700

    add celery task
    send email

--- demo_work/accounts/tests.py
+++ demo_work/accounts/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.urls import reverse
 
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from accounts.models import Shop, CustomUser, Contact
 # Create your tests here.
+
+class AccountTests(APITestCase):
+    def test_create_account(self):
+        url = reverse('registration')
+        data = {
+            'email': 'email@mail.ru',
+            'password': '12345678qwerty',
+            'company': 'ООО "ППР"',
+            'position': 'manager',
+            'username': 'pupkin'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(CustomUser.objects.get().email, 'email@mail.ru')
+
+    # def test_
--- demo_work/order/emails.py
+++ demo_work/order/emails.py
@@ -1,18 +1,15 @@
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
-from django.dispatch import receiver, Signal
 
 from accounts.models import CustomUser
 
-new_order = Signal()
 
-
-@receiver(new_order)
-def new_order_signal(user_id, **kwargs):
+def new_order_email(user_id, **kwargs):
     """Отправка письма при изменении статуса заказа"""
     user = CustomUser.objects.get(id=user_id)
     msg = EmailMultiAlternatives(f'Update order\'s status',
                                  'new order',
                                  settings.EMAIL_HOST_USER,
                                  [user.email])
-    msg.send()
+    return msg.send()
+
