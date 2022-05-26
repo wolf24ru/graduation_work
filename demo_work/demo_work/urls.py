b46ec56c9b3144e37a4307b000cacc1c9eb4297e
Nikita <wolf24ru@gmail.com>
2022-05-26 17:38:49 +0700

    add allauth url

--- demo_work/demo_work/urls.py
+++ demo_work/demo_work/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     # Optional UI:
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-
+    path('accounts/', include('allauth.urls')),
 ]
