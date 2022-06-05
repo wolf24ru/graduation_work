88e162b4300fe4fc9e95eebf11015effff85ff2c
Nikita <wolf24ru@gmail.com>
2022-06-05 13:17:36 +0700

    add comment

--- demo_work/demo_work/urls.py
+++ demo_work/demo_work/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('accounts/', include('allauth.urls')),
-
+    # UI сайта
     path('', home, name='home'),
 ]
 
